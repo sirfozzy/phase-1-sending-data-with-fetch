ac6db473343dc8e8ddf1652169818cf5c43ab38d
Jason Antman <antmanjt@gmail.com>
2022-03-23 22:09:11 -0400

    commit tree dce4adb3940dfd780defa391b7f4560b59757062

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+const submitData= function(userName, userEmail) {
+    return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept":"application/json",
+        },
+        body:JSON.stringify({
+            "name":userName,
+            "email":userEmail
+        })
+    })
+    
+    .then(response=> response.json())
+    .then(data=>renderId(data))
+    .catch((error)=> {
+        const span=document.createElement("span")
+        span.innerText=error.message;
+        document.body.append(span)
+    
+})
+
+
+const renderId=data=> {
+    const span=document.createElement("span")
+    span.innerText=data.id;
+    document.body.append(span)
+    }
+}
